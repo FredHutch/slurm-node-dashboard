e56a77075bb73bc471c7ce38b141f858939d2257
Johnathan Lee <70229707+jlee562@users.noreply.github.com>
2024-11-04 15:55:02 -0800

    updates to features ad partitions selector

--- components/nodeCard/nodes.tsx
+++ components/nodeCard/nodes.tsx
@@ -222,8 +222,8 @@ const Nodes = () => {
         handleNodePartitionsChange={handleNodePartitionsChange}
         handleNodeFeatureChange={handleNodeFeatureChange}
         handleColorSchemaChange={handleColorSchemaChange}
-        partitions={[]}
-        features={[]}
+        partitions={uniquePartitions}
+        features={uniqueFeatures}
         colorSchema={colorSchema}
       />
       <div className="flex justify-between">
